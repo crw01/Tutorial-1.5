d0da9450383db6574bd55c3111290ef67bd81d18
crw01 <crwhitfield@knights.ucf.edu>
2023-02-28 10:37:50 -0500

    Final Anim

--- Tutorial 1.5/Assets/Scripts/CatControl.cs
+++ Tutorial 1.5/Assets/Scripts/CatControl.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CatControl : MonoBehaviour
 {
-   public AudioClip musicClipOne;
+  public AudioClip musicClipOne;
+  public AudioClip musicClipTwo;
+  public AudioSource musicSource;
 
-public AudioClip musicClipTwo;
+  Animator anim;
 
-public AudioSource musicSource;
+void Start()
+{
+  anim = GetComponent<Animator>();
+}
+
 
 // Update is called once per frame
 void Update()
@@ -17,25 +23,26 @@ void Update()
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
-
+          anim.SetInteger("State", 1);
          }
 
      if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
          }
 
      if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
          }
 
      if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
          }
 
      if (Input.GetKeyDown(KeyCode.L))
